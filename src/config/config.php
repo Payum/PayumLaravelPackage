504dbaff441a5220e7da761dc3811eb520cf058c
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-06-23 08:39:48 +0000

    update config example.

--- src/config/config.php
+++ src/config/config.php
@@ -1,23 +1,46 @@
 <?php
 
+use Buzz\Client\Curl;
+use Omnipay\Common\GatewayFactory;
+use Payum\Core\Storage\FilesystemStorage;
+use Payum\LaravelPackage\Action\ObtainCreditCardAction;
+use Payum\Paypal\ExpressCheckout\Nvp\Api;
+use Payum\Paypal\ExpressCheckout\Nvp\PaymentFactory as PaypalPaymentFactory;
+use Payum\OmnipayBridge\PaymentFactory as OmnipayPaymentFactory;
+
 $detailsClass = 'Payum\Core\Model\ArrayObject';
 $tokenClass = 'Payum\Core\Model\Token';
 
+
+// Stripe Payment
+$gatewayFactory = new GatewayFactory;
+$gatewayFactory->find();
+
+$stripeGateway = $gatewayFactory->create('Stripe');
+$stripeGateway->setApiKey($_SERVER['payum.stripe.api_key']);
+$stripeGateway->setTestMode(true);
+
+$stripePayment = OmnipayPaymentFactory::create($stripeGateway);
+$stripePayment->addAction(new ObtainCreditCardAction);
+
+// Paypal Payment
+
+$paypalPayment = PaypalPaymentFactory::create(new Api(new Curl, array(
+    'username' => $_SERVER['payum.paypal_express_checkout.username'],
+    'password' => $_SERVER['payum.paypal_express_checkout.password'],
+    'signature' => $_SERVER['payum.paypal_express_checkout.signature'],
+    'sandbox' => true
+)));
+
 return array(
     // You can pass on object or a service id from container.
-    'token_storage' => new \Payum\Core\Storage\FilesystemStorage(__DIR__.'/../../../../storage/payments', $tokenClass, 'hash'),
+    'token_storage' => new FilesystemStorage(__DIR__.'/../../../../storage/payments', $tokenClass, 'hash'),
     'payments' => array(
-        // Put here any payment you want too, omnipay, payex, paypa, be2bill or any other. Here's example of paypal:
-        //'paypal_es' => \Payum\Paypal\ExpressCheckout\Nvp\PaymentFactory::create(
-        //    new \Payum\Paypal\ExpressCheckout\Nvp\Api(new \Buzz\Client\Curl, array(
-        //        'username' => 'REPLACE WITH YOURS',
-        //        'password' => 'REPLACE WITH YOURS',
-        //        'signature' => 'REPLACE WITH YOURS',
-        //        'sandbox' => true
-        //    ))
-        //),
+        // Put here any payment you want too, omnipay, payex, paypa, be2bill or any other. Here's example of paypal and stripe:
+//        'paypal_es' => $paypalPayment,
+//        'omnipay_stripe' => $stripePayment,
     ),
     'storages' => array(
-        $detailsClass => new \Payum\Core\Storage\FilesystemStorage(__DIR__.'/../../../../storage/payments', $detailsClass),
+        $detailsClass => new FilesystemStorage(__DIR__.'/../../../../storage/payments', $detailsClass),
     )
 );
