735c02f41403677295036e4330cdb852a89e7669
André Teixeira <andre.teixeira@gmail.com>
2014-10-28 22:33:42 -0200

    Creating a RefundController

---
+++ src/Payum/LaravelPackage/Controller/RefundController.php
@@ -0,0 +1,25 @@
+<?php
+namespace Payum\LaravelPackage\Controller;
+
+use Payum\Core\Request\Refund;
+use Symfony\Component\HttpFoundation\Request;
+
+class RefundController extends PayumController
+{
+    public function doAction($payum_token)
+    {
+        /** @var Request $request */
+        $request = \App::make('request');
+        $request->attributes->set('payum_token', $payum_token);
+
+        $token = $this->getHttpRequestVerifier()->verify($request);
+
+        $payment = $this->getPayum()->getPayment($token->getPaymentName());
+
+        $payment->execute(new Refund($token));
+
+        $this->getHttpRequestVerifier()->invalidate($token);
+
+        return \Redirect::to($token->getAfterUrl());
+    }
+}
